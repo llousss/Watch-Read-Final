1562a1e48e0b3888f3fff95e6ea533ae552e0056
llousss <matheusgiacomello13@gmail.com>
2025-12-01 14:06:41 -0300

    porta 3309 para a conexão na faculdade

--- db.php
+++ db.php
@@ -3,8 +3,9 @@ $host = "localhost";
 $user = "root";
 $pass = "";
 $dbname = "watchread";
+$port = 3309;
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
 
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
